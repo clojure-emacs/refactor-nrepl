f3f184b9e61b52d8b88b65d9b84d9e572f1b95ef
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-07-06 15:26:31 +0100

    adds missing profiles

--- project.clj
+++ project.clj
@@ -7,4 +7,7 @@
                  [org.clojure/tools.analyzer.jvm "0.2.2"]
                  [org.clojure/tools.namespace "0.2.4"]
                  [org.clojure/tools.reader "0.8.4"]]
-  :profiles {:test {:dependencies [[print-foo "0.5.3"]]}})
+  :profiles {:test {:dependencies [[print-foo "0.5.3"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}})
