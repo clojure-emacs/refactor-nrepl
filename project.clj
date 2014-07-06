b5f321d52003bed64d8076b6fe53062b7b407a1e
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-07-06 15:58:48 +0100

    adds snapshot repo

--- project.clj
+++ project.clj
@@ -10,4 +10,5 @@
   :profiles {:test {:dependencies [[print-foo "0.5.3"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :dev {:repositories [["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]}})
