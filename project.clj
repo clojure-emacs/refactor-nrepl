4d6f9bb8274a1aa926935191f2d8910f242c6f58
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-09-22 22:33:07 +0100

    uses mranderson for dependencies
    - so dependencies can be included as source and dependency clashes can
      be avoided with projects being refactored

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "nREPL middleware to support refactorings in an editor agnostic way"
   :url "http://github.com/clojure-emacs/refactor-nrepl"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.3"]
-                 [org.clojure/tools.analyzer.jvm "0.5.4"]
-                 [org.clojure/tools.namespace "0.2.5"]
-                 [org.clojure/tools.reader "0.8.5"]]
+                 ^:source-dep [org.clojure/tools.namespace "0.2.5"]
+                 ^:source-dep [org.clojure/tools.reader "0.8.5"]
+                 ^:source-dep [org.clojure/tools.analyzer.jvm "0.5.4"]
+                 ^:source-dep [org.clojure/tools.nrepl "0.2.3"]]
+  :plugins [[thomasa/mranderson "0.1.0"]]
   :profiles {:test {:dependencies [[print-foo "0.5.3"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
