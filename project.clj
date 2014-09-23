86ca1542eda6f53cd526a39cc982201179496a94
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-09-23 15:54:05 +0100

    nrepl can not be mrandersoned
    - see benedekfazekas/mranderson#1 for details

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  ^:source-dep [org.clojure/tools.namespace "0.2.5"]
                  ^:source-dep [org.clojure/tools.reader "0.8.5"]
                  ^:source-dep [org.clojure/tools.analyzer.jvm "0.5.4"]
-                 ^:source-dep [org.clojure/tools.nrepl "0.2.3"]]
+                 [org.clojure/tools.nrepl "0.2.3"]]
   :plugins [[thomasa/mranderson "0.2.1"]]
   :profiles {:test {:dependencies [[print-foo "0.5.3"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
