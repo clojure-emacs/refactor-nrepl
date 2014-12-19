e7d3257fbef1de7ff0d68325d969eb9a9548dfc5
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-12-19 16:42:14 +0000

    updates mranderson version

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  ^:source-dep [org.clojure/tools.analyzer.jvm "0.6.5"]
                  ^:source-dep [org.clojure/tools.namespace "0.2.7"]
                  ^:source-dep [org.clojure/tools.reader "0.8.12"]]
-  :plugins [[thomasa/mranderson "0.2.1"]]
+  :plugins [[thomasa/mranderson "0.2.2"]]
   :filespecs [{:type :bytes :path "refactor-nrepl/refactor-nrepl/project.clj" :bytes ~(slurp "project.clj")}]
   :profiles {:test {:dependencies [[print-foo "1.0.1"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
