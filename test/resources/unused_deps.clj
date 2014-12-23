41a35335171e266d01eb55ebc86a92a5cd4b5e7f
Benedek Fazekas <benedek.fazekas@mailonline.co.uk>
2014-12-23 08:37:40 +0000

    fixes test mrandersoned failure
    - clojure.tools.reader gets inlined by mranderson so can't be resolved

--- test/resources/unused_deps.clj
+++ test/resources/unused_deps.clj
@@ -1,7 +1,6 @@
 (ns resources.unused-deps
   (:require [clojure edn instant
              [string :as str :refer [join]]]
-            [clojure.tools.reader :as r]
             [clojure.test :refer :all])
   (:import java.util.Deque
            java.util.FormattableFlags
