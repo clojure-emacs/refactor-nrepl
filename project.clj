c538a600f6ef01b98f0dae1a51a95d695b62ffc1
Lars Andersen <expez@expez.com>
2015-03-05 08:12:23 +0100

    bump tools.nrepl to 0.2.7
    cider will warn when older versions are in use, for now leiningen is the
    likely culprit when the user ends up with an old version but when that
    changes we shouldn't be standing in the way of progress :)

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.nrepl "0.2.7"]
                  ^:source-dep [http-kit "2.1.19"]
                  [org.clojure/data.json "0.2.5"]
                  ^:source-dep [instaparse "1.3.4"]
