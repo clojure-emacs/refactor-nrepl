c013ef5624bbedaf861bba3de2931ad678352042
Lars Andersen <expez@expez.com>
2015-03-16 14:50:25 +0100

    Find clojure jar on cp and require namespaces in bootstrap

--- src/refactor_nrepl/bootstrap.clj
+++ src/refactor_nrepl/bootstrap.clj
@@ -1,8 +1,10 @@
 (ns refactor-nrepl.bootstrap
   (:require [alembic.still :refer [distill make-still]]
-            [classlojure.core :refer [base-classloader]]
+            [classlojure.core
+             :refer [classlojure eval-in get-classpath base-classloader]]
             [clojure.edn :as edn]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io])
+  (:import java.io.File))
 
 (def repositories {"clojars" "http://clojars.org/repo"
                    "central" "http://repo1.maven.org/maven2/"})
@@ -16,8 +18,29 @@
                       second)]
     (-> profiles :provided :dependencies)))
 
+(defn- clojure-jar-URL []
+  (->> base-classloader
+       get-classpath
+       (filter #(re-find #"clojure-\d.\d.\d.jar" %))
+       first
+       File.
+       .toURL))
+
 (defn init-classloader []
-  (let [still (atom (make-still base-classloader))]
-    (doseq [dep (core-dependencies)]
+  (let [still (atom (make-still (classlojure (clojure-jar-URL))))]
+    (doseq [dep (conj (core-dependencies)
+                      ['refactor-nrepl-core "0.3.0-SNAPSHOT"])]
       (distill dep :repositories repositories :still still))
+    (eval-in (:classloader @still)
+             '(do
+                (require
+                 '[refactor-nrepl-core.ns
+                   [clean-ns :refer [clean-ns]]
+                   [pprint :refer [pprint-ns]]
+                   [resolve-missing :refer [resolve-missing]]])
+                (require
+                 '[refactor-nrepl-core
+                   [analyzer :refer [find-unbound-vars]]
+                   [artifacts :refer [artifact-versions artifacts-list]]
+                   [find-symbol :refer [find-debug-fns find-symbol]]])))
     (:classloader @still)))
