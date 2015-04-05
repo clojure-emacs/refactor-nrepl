38e5ba926b73b43f087a6b81dab5fa7126be129e
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-04-05 16:52:50 +0100

    upgrade to clj1.7alpha6 for testing

--- project.clj
+++ project.clj
@@ -20,7 +20,7 @@
              :test {:dependencies [[print-foo "1.0.1"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha6"]]}
              :dev {:plugins [[jonase/eastwood "0.2.0"]]
                    :dependencies [[me.raynes/fs "1.4.6"]]
                    :resource-paths ["test/resources"
