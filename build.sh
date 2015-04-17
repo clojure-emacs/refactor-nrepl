489bf62c5c8d90f61e64ff6cca616e28c8107782
Benedek Fazekas <benedek.fazekas@gmail.com>
2015-04-17 21:17:40 +0100

    exit if a step fails

--- build.sh
+++ build.sh
@@ -2,6 +2,15 @@
 
 # runs source-deps and tests and then provided lein target with mranderson
 
+function check_result {
+    if [ $? -ne 0 ]; then
+        echo "FAILED"
+        exit 1
+    fi
+}
+
 lein do clean, source-deps :prefix-exclusions "[\"classlojure\"]"
+check_result
 lein with-profile +plugin.mranderson/config test
+check_result
 lein with-profile plugin.mranderson/config "$@"
