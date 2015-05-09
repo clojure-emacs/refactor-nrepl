706263c4b1e2bae678003afd6f5a48f8d36d62f9
Lars Andersen <expez@expez.com>
2015-05-09 20:47:17 +0200

    Refactor tests for get-enclosing-sexp
    This had ACTUAL EXPECTED insetad of EXPECTED ACTUAL which made it harder
    to read the diffs.

--- test/refactor_nrepl/util_test.clj
+++ test/refactor_nrepl/util_test.clj
@@ -3,8 +3,8 @@
             [clojure.test :refer :all]))
 
 (def file-content (slurp "test/resources/testproject/src/com/example/sexp_test.clj"))
-(def weird-file-content ";; some weird file
-;; not even clojure
+  (def weird-file-content ";; some weird file
+  ;; not even clojure
 ;; perhaps? no parens!")
 (def binding-location [4 10])
 (def funcall-location [7 8])
@@ -13,11 +13,10 @@
 (def weird-location [2 5])
 
 (deftest get-enclosing-sexp-test
-  (is (= (apply get-enclosing-sexp file-content binding-location)
-         "[some :bindings
-        more :bindings]"))
-  (is (= (apply get-enclosing-sexp file-content funcall-location)
-         "(println #{some}
+  (is (= "[some :bindings
+        more :bindings]"
+         (apply get-enclosing-sexp file-content binding-location)))
+  (is (= "(println #{some}
              ;; helpful comment
              (prn {\"foo\" {:qux [#{more}]}}))"))
   (is (= (apply get-enclosing-sexp file-content set-location) "#{more}"))
