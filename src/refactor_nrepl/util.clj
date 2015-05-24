f8b4c4d04263bb07b4ad56edb72bcd0c233667df
Lars Andersen <expez@expez.com>
2015-05-24 11:18:56 +0200

    Fix duplicate definitions in util after bad merge
    When rebasing on the windows machine at work git inserts ^M everywhere
    and that wreaks complete havoc on any merge diffs.
    
    The previous versions appeared earlier in the file so they were being
    overwritten by the more recent versions so all the tests passed.

--- src/refactor_nrepl/util.clj
+++ src/refactor_nrepl/util.clj
@@ -37,67 +37,11 @@
     (throw (IllegalArgumentException.
             "Refactor nrepl doesn't work on cljs or cljx files!"))))
 
-(defn- search-sexp-boundary
-  "Searches sexp open or close of depth depending on the passed in pred.
-
-   If search is to be done backwards pass the string in reversed."
-  ([pred s]
-   (let [open #{\[ \{ \(}
-         close #{\] \} \)}]
-     (if (some (set/union open close) s)
-       (loop [chars s
-              cnt 0
-              op-cl 0
-              ready false]
-         (let [[ch & rest-chars] chars]
-           (cond (and ready (pred op-cl))
-                 (if (and (= (nth s cnt) \#)
-                          (= (nth s (dec cnt) \{)))
-                   (inc cnt)
-                   cnt)
-
-                 (open ch)
-                 (recur rest-chars (inc cnt) (inc op-cl) true)
-
-                 (close ch)
-                 (recur rest-chars (inc cnt) (dec op-cl) true)
-
-                 :else
-                 (recur rest-chars (inc cnt) op-cl ready))))
-       0))))
-
 (defn- read-first-form [form]
   (let [f-string (str form)]
     (when (some #{\) \} \]} f-string)
       (-> f-string
           read-string))))
-
-(defn get-enclosing-sexp
-  "Extracts the sexp enclosing point at LINE and COLUMN in FILE-CONTENT.
-
-  A string is not treated as a sexp by this function.
-
-  Line is indexed from 1, and column is indexed from 0 (this is how
-  emacs does it)."
-  [file-content line column]
-  (let [lines (str/split-lines file-content)
-        line-index (dec line)
-        char-count-for-lines (->> lines
-                                  (take line-index)
-                                  (map count)
-                                  (reduce + line-index))
-        content-to-point (-> char-count-for-lines
-                             (+ column)
-                             (take file-content))
-        sexp-start (->> content-to-point
-                        reverse
-                        (search-sexp-boundary (partial < 0))
-                        (- (count content-to-point)))
-        content-from-sexp (.substring file-content sexp-start)
-        sexp-end (+ sexp-start
-                    (->> content-from-sexp
-                         (search-sexp-boundary (partial = 0))))]
-    (.substring file-content sexp-start sexp-end)))
 
 (defn node-for-sexp?
   "Is NODE the ast node for SEXP?"
