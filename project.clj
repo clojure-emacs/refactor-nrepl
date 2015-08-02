0caa330c79b364b68c74e4e734cb445770c3c6a6
Lars Andersen <expez@expez.com>
2015-08-02 13:00:56 +0200

    Bump tools.reader to 0.9.2

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  ^:source-dep [alembic "0.3.2"]
                  ^:source-dep [org.clojure/tools.analyzer.jvm "0.6.6"]
                  ^:source-dep [org.clojure/tools.namespace "0.2.10"]
-                 ^:source-dep [org.clojure/tools.reader "0.8.12"]
+                 ^:source-dep [org.clojure/tools.reader "0.9.2"]
                  ^:source-dep [org.clojure/java.classpath "0.2.2"]
                  ^:source-dep [me.raynes/fs "1.4.6"]
                  ^:source-dep [rewrite-clj "0.4.12"]]
