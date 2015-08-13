0fa3f956eec967a36147d1c03ca0132757644702
Lars Andersen <expez@expez.com>
2015-08-13 11:16:04 +0200

    Bump tools.namespace dep
    0.2.11 adds support for reader conditionals.  This might keep
    move-file-or-dir from breaking on projects containing cljc files.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  ^:source-dep [cheshire "5.4.0"]
                  ^:source-dep [alembic "0.3.2"]
                  ^:source-dep [org.clojure/tools.analyzer.jvm "0.6.6"]
-                 ^:source-dep [org.clojure/tools.namespace "0.2.10"]
+                 ^:source-dep [org.clojure/tools.namespace "0.2.11"]
                  ^:source-dep [org.clojure/tools.reader "0.9.2"]
                  ^:source-dep [org.clojure/java.classpath "0.2.2"]
                  ^:source-dep [me.raynes/fs "1.4.6"]
