5e3d1bb08922f870fd0976644ec932d4b3af7879
Lars Andersen <expez@expez.com>
2015-11-21 09:57:08 +0100

    Bump cider and nrepl deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/clojure-emacs/refactor-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/clojure "1.7.0"]
                  ^:source-dep [http-kit "2.1.19"]
                  ^:source-dep [cheshire "5.4.0"]
@@ -19,7 +19,7 @@
                  ^:source-dep [version-clj "0.1.2"]]
   :plugins [[thomasa/mranderson "0.4.6"]]
   :filespecs [{:type :bytes :path "refactor-nrepl/refactor-nrepl/project.clj" :bytes ~(slurp "project.clj")}]
-  :profiles {:provided {:dependencies [[cider/cider-nrepl "0.9.0"]]}
+  :profiles {:provided {:dependencies [[cider/cider-nrepl "0.9.1"]]}
              :test {:dependencies [[print-foo "1.0.1"]]
                     :src-paths ["test/resources"]}
              :dev {:plugins [[jonase/eastwood "0.2.0"]]
