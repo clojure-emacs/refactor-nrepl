041675ede4d30f471e5a67f49749aa11b8e31137
Benedek Fazekas <benedek.fazekas@gmail.com>
2016-02-03 20:43:38 +0000

    [Fix #140] move clj1.7 to provided dep scope
    additionally add 1.8 as a test target

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                 [org.clojure/clojure "1.7.0"]
+
                  ^:source-dep [http-kit "2.1.19"]
                  ^:source-dep [cheshire "5.4.0"]
                  ^:source-dep [alembic "0.3.2"]
@@ -19,9 +19,11 @@
                  ^:source-dep [version-clj "0.1.2"]]
   :plugins [[thomasa/mranderson "0.4.6"]]
   :filespecs [{:type :bytes :path "refactor-nrepl/refactor-nrepl/project.clj" :bytes ~(slurp "project.clj")}]
-  :profiles {:provided {:dependencies [[cider/cider-nrepl "0.10.0"]]}
+  :profiles {:provided {:dependencies [[cider/cider-nrepl "0.10.0"]
+                                       [org.clojure/clojure "1.7.0"]]}
              :test {:dependencies [[print-foo "1.0.1"]]
                     :src-paths ["test/resources"]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :dev {:plugins [[jonase/eastwood "0.2.0"]]
                    :dependencies [[org.clojure/clojurescript "1.7.48"]
                                   [com.cemerick/piggieback "0.2.1"]
