509769a63d1336b7c4fbb6bdbaf9015ecde35e24
Lars Andersen <expez@expez.com>
2016-12-15 15:14:12 +0100

    Warn and disable when not in project

--- src/refactor_nrepl/plugin.clj
+++ src/refactor_nrepl/plugin.clj
@@ -14,28 +14,32 @@
 
 (defn middleware
   [{:keys [dependencies exclusions] :as project}]
-  (let [clojure-excluded? (some #(= % 'org.clojure/clojure) exclusions)
-        clojure-version (when-not clojure-excluded?
-                          (find-clojure-version dependencies))
-        clojure-version-ok?
-        (cond clojure-excluded? true ; up to the user
+  (if (core/project-root)
+    (let [clojure-excluded? (some #(= % 'org.clojure/clojure) exclusions)
+          clojure-version (when-not clojure-excluded?
+                            (find-clojure-version dependencies))
+          clojure-version-ok?
+          (cond clojure-excluded? true ; up to the user
 
-              (nil? clojure-version)
-              ;; Lein 2.5.2+ uses Clojure 1.7 by default
-              (lein/version-satisfies? (lein/leiningen-version) "2.5.2")
+                (nil? clojure-version)
+                ;; Lein 2.5.2+ uses Clojure 1.7 by default
+                (lein/version-satisfies? (lein/leiningen-version) "2.5.2")
 
-              :else
-              (lein/version-satisfies? clojure-version min-clojure-version))]
-    (if clojure-version-ok?
-      (-> project
-          (update-in [:dependencies]
-                     (fnil into [])
-                     [['refactor-nrepl (core/version)]])
-          (update-in [:repl-options :nrepl-middleware]
-                     (fnil into [])
-                     '[refactor-nrepl.middleware/wrap-refactor]))
-      (do
-        (lein/warn "Warning: refactor-nrepl requires Clojure version"
-                   min-clojure-version "or later.")
-        (lein/warn "Warning: refactor-nrepl middleware won't be activated." )
-        project))))
+                :else
+                (lein/version-satisfies? clojure-version min-clojure-version))]
+      (if clojure-version-ok?
+        (-> project
+            (update-in [:dependencies]
+                       (fnil into [])
+                       [['refactor-nrepl (core/version)]])
+            (update-in [:repl-options :nrepl-middleware]
+                       (fnil into [])
+                       '[refactor-nrepl.middleware/wrap-refactor]))
+        (do
+          (lein/warn "Warning: refactor-nrepl requires Clojure version"
+                     min-clojure-version "or later.")
+          (lein/warn "Warning: refactor-nrepl middleware won't be activated." )
+          project)))
+    (do
+      (lein/warn "Warning: refactor-nrepl needs to run in the context of a project.")
+      (lein/warn "Warning: refactor-nrepl middleware won't be activated." ))))
