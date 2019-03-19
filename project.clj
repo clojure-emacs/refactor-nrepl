9e85be8708280a27c5f332bff1a016ce6790df66
Shen Tian <biozinc@gmail.com>
2019-03-19 21:43:53 +0200

    Sonatype in https mode?

--- project.clj
+++ project.clj
@@ -41,5 +41,5 @@
                    :java-source-paths ["test/java"]
                    :resource-paths ["test/resources"
                                     "test/resources/testproject/src"]
-                   :repositories [["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]]}}
+                   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]]}}
   :jvm-opts ["-Djava.net.preferIPv4Stack=true"])
