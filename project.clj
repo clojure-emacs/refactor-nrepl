56eeebfb25ab54be23588d7e015fc36275ca3692
Bozhidar Batsov <bozhidar@batsov.com>
2021-02-16 10:47:12 +0200

    Bump orchard

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  ^:inline-dep [org.clojure/tools.analyzer.jvm "1.1.0"]
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0" :exclusions [org.clojure/tools.reader]]
                  ^:inline-dep [org.clojure/tools.reader "1.3.5"]
-                 ^:inline-dep [cider/orchard "0.5.6"]
+                 ^:inline-dep [cider/orchard "0.6.5"]
                  ^:inline-dep [cljfmt "0.7.0"]
                  ^:inline-dep [clj-commons/fs "1.6.307"]
                  ^:inline-dep [rewrite-clj "0.6.1"]
