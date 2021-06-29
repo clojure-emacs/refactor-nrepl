dbde10c3087d0334fd80fa0c65b46e7fd9c534c8
Lars Andersen <expez@expez.com>
2021-06-29 11:43:46 +0200

    [Fix #312] Local tests without mranderson

--- test/global_test_setup.clj
+++ test/global_test_setup.clj
@@ -1,7 +1,11 @@
 (ns global-test-setup
-  "This namespace's contents will be automatically loaded by `lein test` (or any test runner)."
-  (:require
-   [refactor-nrepl.inlined-deps.toolsnamespace.v1v1v0.clojure.tools.namespace.repl :refer [set-refresh-dirs]]))
+  "This namespace's contents will be automatically loaded by `lein test` (or any test runner).")
+
+(try
+  (require '[refactor-nrepl.inlined-deps.toolsnamespace.v1v1v0.clojure.tools.namespace.repl :refer [set-refresh-dirs]])
+  (catch Exception _
+    (require
+     '[clojure.tools.namespace.repl :refer [set-refresh-dirs]])))
 
 (when (System/getenv "CI") ; don't alter local environments (which may also use clojure.tools.namespace.repl)
   ;; Intentionally exclude the `lein-plugin/` source path:
