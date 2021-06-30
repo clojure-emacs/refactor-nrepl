fddd9adf8ea50072963151f313ded6b32ccbeff7
Lars Andersen <expez@expez.com>
2021-07-01 01:52:33 +0200

    Replace cheshire with data.json
    Fixes #306

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[nrepl "0.8.3"]
                  ^:inline-dep [http-kit "2.5.1"]
-                 ^:inline-dep [cheshire "5.10.0"]
+                 ^:inline-dep [org.clojure/data.json "2.3.1"]
                  ^:inline-dep [org.clojure/tools.analyzer.jvm "1.1.0"]
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0" :exclusions [org.clojure/tools.reader]]
                  ^:inline-dep [org.clojure/tools.reader "1.3.5"]
@@ -48,7 +48,6 @@
              :test {:dependencies [[print-foo "1.0.2"]]}
              :dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[org.clojure/clojurescript "1.10.520"]
-                                  [javax.xml.bind/jaxb-api "2.3.1"]
                                   [cider/piggieback "0.5.2"]
                                   [commons-io/commons-io "2.8.0"]]
                    :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
