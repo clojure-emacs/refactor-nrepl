12e5cea3a49b2cf754c9547b42f27a361e87611a
vemv <vemv@users.noreply.github.com>
2021-09-25 06:58:08 +0200

    Satisfy clj-kondo

--- test/refactor_nrepl/ns/slam/hound/regrow_test.clj
+++ test/refactor_nrepl/ns/slam/hound/regrow_test.clj
@@ -4,10 +4,10 @@
    [clojure.test :refer [deftest is]]))
 
 (deftest works
-  (is ('#{#{java.lang.Thread sun.jvm.hotspot.runtime.Thread}
-          #{java.lang.Thread}}
-       (sut/candidates :import 'Thread [] {})))
+  (is (contains? '#{#{java.lang.Thread sun.jvm.hotspot.runtime.Thread}
+                    #{java.lang.Thread}}
+                 (sut/candidates :import 'Thread [] {})))
 
-  (is ('#{#{clojure.core}
-          #{clojure.core cljs.core}}
-       (sut/candidates :refer 'areduce [] {}))))
+  (is (contains? '#{#{clojure.core}
+                    #{clojure.core cljs.core}}
+                 (sut/candidates :refer 'areduce [] {}))))
