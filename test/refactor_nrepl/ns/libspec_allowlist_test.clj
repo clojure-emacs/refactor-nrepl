5f2b6c2c0619c706487675fc2d51f53d66d42ffc
Vadim Rodionov <rodionov@tuta.io>
2022-10-16 13:36:37 +0600

    Extend tests for `refactor-nrepl.ns.libspec-allowlist`

--- test/refactor_nrepl/ns/libspec_allowlist_test.clj
+++ test/refactor_nrepl/ns/libspec_allowlist_test.clj
@@ -1,8 +1,13 @@
 (ns refactor-nrepl.ns.libspec-allowlist-test
+  {:clj-kondo/config '{:linters {:unused-namespace {:exclude [something.important
+                                                              "something.more.important*"]}}}}
   (:require
    [clojure.test :refer [are deftest is testing]]
    [refactor-nrepl.ns.libspec-allowlist :as sut]
-   [refactor-nrepl.ns.prune-dependencies :as prune-dependencies]))
+   [refactor-nrepl.ns.prune-dependencies :as prune-dependencies]
+   [refactor-nrepl.ns.ns-parser :refer [parse-ns]]))
+
+(def this-ns (parse-ns "test/refactor_nrepl/ns/libspec_allowlist_test.clj"))
 
 (deftest libspec-allowlist-test
   (testing "Takes into account refactor-nrepls own config, and .clj-kondo/config files alike,
@@ -12,26 +17,41 @@ merging their results"
             ;; from our .clj-kondo file - symbols become quoted patterns:
             "^\\Qsample.unused.namespace\\E$"
             ;; from our .clj-kondo file - strings have 'regex' semantics so are kept as-is:
-            "more.unused.namespaces*"]
+            "more.unused.namespaces*"
+            ;; from our .clj-konfo file, namespace local configuration
+            "^\\Qreally.important\\E$"
+            "^\\Qanother.important*\\E$"
+            ;; from attr-map of this ns
+            "^\\Qsomething.important\\E$"
+            "something.more.important*"]
 
-           (sut/libspec-allowlist)))
+           (sut/libspec-allowlist this-ns)))
 
-    (is (every? string? (sut/libspec-allowlist))
+    (is (every? string? (sut/libspec-allowlist nil))
         "Items coming from different sources all have the same class,
 ensuring they will be treated homogeneously by refactor-nrepl")
 
     (testing "`libspec-should-never-be-pruned?` is integrated with clj-kondo logic,
 effecively parsing its config into well-formed regexes"
       (are [input expected] (= expected
-                               (prune-dependencies/libspec-should-never-be-pruned? {:ns input}))
+                               (prune-dependencies/libspec-should-never-be-pruned? this-ns {:ns input}))
         'sample.unused.namespace   true
         'Asample.unused.namespace  false
         'sample.unused.namespaceB  false
         'more.unused.namespaces    true
         'more.unused.namespacessss true
-        'more.unused.namespac      false))
+        'more.unused.namespac      false
+        'really.important          true
+        'really.importante         false
+        'another.important         true
+        'another.importante        true
+        'Banother.important        false
+        'something.important       true
+        'Esomething.important      false
+        'something.more.important  true
+        'something.more.importante true))
 
     (testing "Always returns a sequence, memoized or not"
       (is (seq (sut/with-memoized-libspec-allowlist
-                 (sut/libspec-allowlist))))
-      (is (seq (sut/libspec-allowlist))))))
+                 (sut/libspec-allowlist this-ns))))
+      (is (seq (sut/libspec-allowlist this-ns))))))
