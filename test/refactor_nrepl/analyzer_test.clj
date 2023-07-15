986f86edf22e514ddf2135627e6f435278e29f34
vemv <vemv@users.noreply.github.com>
2023-07-15 12:33:15 +0200

    Add test

--- test/refactor_nrepl/analyzer_test.clj
+++ test/refactor_nrepl/analyzer_test.clj
@@ -1,6 +1,7 @@
 (ns refactor-nrepl.analyzer-test
   (:require
    [clojure.java.io :as io]
+   [clojure.pprint :as pprint]
    [clojure.test :refer [deftest is]]
    [refactor-nrepl.analyzer :as sut]))
 
@@ -9,3 +10,14 @@
              "clashing_defprotocol_method_name.clj"]
           :let [c (-> f io/resource slurp)]]
     (is (some? (sut/ns-ast c)))))
+
+(deftest warm-ast-cache-test
+  (let [pairs (partition 2 (sut/warm-ast-cache))]
+    (when (System/getenv "CI")
+      (pprint/pprint pairs))
+    (doseq [[ns-sym result] pairs]
+      (is (simple-symbol? ns-sym))
+      (is (or (= "OK" result)
+              (and (list? result)
+                   (-> result first #{"error"})
+                   (-> result second string?)))))))
