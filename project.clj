e7a67846b03f71c1a09e87f04a925f7617202cd6
Oleksandr Yakushev <alex@bytopia.org>
2025-05-30 14:29:52 +0300

    Stop depending on Compliment

--- project.clj
+++ project.clj
@@ -7,13 +7,12 @@
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[nrepl "1.3.1" :exclusions [org.clojure/clojure]]
-                 ^:inline-dep [compliment "0.7.0"]
                  ^:inline-dep [http-kit "2.5.0"]
                  ^:inline-dep [org.clojure/data.json "2.5.0"]
                  ^:inline-dep [org.clojure/tools.analyzer.jvm "1.3.2"]
                  ^:inline-dep [org.clojure/tools.namespace "1.5.0" :exclusions [org.clojure/tools.reader]]
                  ^:inline-dep [org.clojure/tools.reader "1.5.2"]
-                 ^:inline-dep [cider/orchard "0.34.3" :exclusions [org.clojure/clojure]]
+                 ^:inline-dep [cider/orchard "0.35.0" :exclusions [org.clojure/clojure]]
                  ^:inline-dep [cljfmt "0.9.2" :exclusions [rewrite-clj rewrite-cljs]]
                  ^:inline-dep [clj-commons/fs "1.6.310"]
                  ^:inline-dep [rewrite-clj "1.1.49"]
