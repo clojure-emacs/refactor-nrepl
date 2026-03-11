e964245b5b97a17837ce16f9bbd957d383b5a6ce
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-11 09:45:11 +0200

    Bump tools.reader to 1.6.0

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  ^:inline-dep [org.clojure/data.json "2.5.2"]
                  ^:inline-dep [org.clojure/tools.analyzer.jvm "1.3.4"]
                  ^:inline-dep [org.clojure/tools.namespace "1.5.1" :exclusions [org.clojure/tools.reader]]
-                 ^:inline-dep [org.clojure/tools.reader "1.5.2"]
+                 ^:inline-dep [org.clojure/tools.reader "1.6.0"]
                  ^:inline-dep [cider/orchard "0.35.0" :exclusions [org.clojure/clojure]]
                  ^:inline-dep [cljfmt "0.9.2" :exclusions [rewrite-clj rewrite-cljs]]
                  ^:inline-dep [clj-commons/fs "1.6.310"]
